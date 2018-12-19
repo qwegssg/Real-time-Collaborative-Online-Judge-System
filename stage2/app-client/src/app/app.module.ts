5e7df5c96336f0acced3906d2b32f60133a65723
qwegssg <yutaoren118@gmail.com>
2018-12-19 06:17:03 -0800

    create stage2

---
+++ stage2/app-client/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ProblemListComponent } from './components/problem-list/problem-list.component';
+import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
+
+import { DataService} from "./services/data.service";
+
+import { routing } from "./app.routes"
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProblemListComponent,
+    ProblemDetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    routing
+  ],
+  // providers provide the module that can be used everywhere
+  providers: [{
+    provide: "data",
+    useClass: DataService
+  }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ stage2/app-client/src/app/components/problem-detail/problem-detail.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { Problem } from '../../models/problem.model';
+import { ActivatedRoute } from "@angular/router";
+
+@Component({
+  selector: 'app-problem-detail',
+  templateUrl: './problem-detail.component.html',
+  styleUrls: ['./problem-detail.component.css']
+})
+export class ProblemDetailComponent implements OnInit {
+
+  problem: Problem;
+
+  constructor(
+    private route: ActivatedRoute,
+    @Inject("data") private data
+  ) { }
+
+
+  ngOnInit() {
+    // subscribe: whenever the params have changed, the function is called
+    this.route.params.subscribe(params => {
+      // +: transfer string into number
+      this.problem = this.data.getProblem(+params['id']);
+    });
+  }
+
+}
