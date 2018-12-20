07da120226c48250e8fdd325e5ef5a60b7ba661e
qwegssg <yutaoren118@gmail.com>
2018-12-20 00:23:39 -0800

    create a form to add new problem

--- stage2/app-client/src/app/app.module.ts
+++ stage2/app-client/src/app/app.module.ts
@@ -1,23 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+
+import { routing } from "./app.routes";
+
+import { DataService} from "./services/data.service";
 
 import { AppComponent } from './app.component';
 import { ProblemListComponent } from './components/problem-list/problem-list.component';
 import { ProblemDetailComponent } from './components/problem-detail/problem-detail.component';
-
-import { DataService} from "./services/data.service";
-
-import { routing } from "./app.routes"
+import { NewProblemComponent } from './components/new-problem/new-problem.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProblemListComponent,
-    ProblemDetailComponent
+    ProblemDetailComponent,
+    NewProblemComponent
   ],
   imports: [
     BrowserModule,
-    routing
+    routing,
+    FormsModule,
+    HttpModule
   ],
   // providers provide the module that can be used everywhere
   providers: [{
--- stage2/app-client/src/app/services/data.service.ts
+++ stage2/app-client/src/app/services/data.service.ts
@@ -5,15 +5,23 @@ import { PROBLEMS } from "../feed-problem";
 @Injectable()
 export class DataService {
 
+  // assign the PROBLEMS array to a new array so that new problems can be added to the array
+  problems: Problem[] = PROBLEMS;
+
   constructor() { }
 
   // get all the problems
   getProblems(): Problem[] {
-    return PROBLEMS;
+    return this.problems;
   }
   // get specific problem by id
   getProblem(id: number): Problem {
-    return PROBLEMS.find((problem) => problem.id === id);
+    return this.problems.find((problem) => problem.id === id);
+  }
+
+  addProblem(problem: Problem): void {
+    problem.id = this.problems.length + 1;
+    this.problems.push(problem);
   }
 
 }
