fb91ae37e2157e3df475f42f38759cafab1ab18a
qwegssg <yutaoren118@gmail.com>
2018-12-27 07:47:30 -0800

    add auth0 login service

---
+++ stage3/app-client/src/app/services/auth.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import * as auth0 from 'auth0-js';
+
+@Injectable()
+export class AuthService {
+
+  private _idToken: string;
+  private _accessToken: string;
+  private _expiresAt: number;
+
+  auth0 = new auth0.WebAuth({
+    clientID: 'hlKNud6NY7DxrPv2s44utjvo69WcsoVc',
+    domain: 'collaborativeoj-yutaoren.auth0.com',
+    responseType: 'token id_token',
+    redirectUri: 'http://localhost:3000',
+    scope: 'openid'
+  });
+
+  constructor(public router: Router) {
+    this._idToken = '';
+    this._accessToken = '';
+    this._expiresAt = 0;
+  }
+
+  get accessToken(): string {
+    return this._accessToken;
+  }
+
+  get idToken(): string {
+    return this._idToken;
+  }
+
+  public login(): void {
+    this.auth0.authorize();
+  }
+
+}
